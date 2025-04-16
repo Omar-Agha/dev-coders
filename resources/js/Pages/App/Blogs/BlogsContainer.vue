008b089e0b33d69e4122310df5c23e8d6a5d2b9e
omar <omar.tamer982@gmail.com>
2025-04-16 16:30:11 +0300

    remove comment

--- resources/js/Pages/App/Blogs/BlogsContainer.vue
+++ resources/js/Pages/App/Blogs/BlogsContainer.vue
@@ -1,38 +1,7 @@
 <template>
     <div class="relative">
         <Blog :image="blog1.image" class="z-20">
-            <!-- <template v-slot:title>
-                Our Process: <span class="gradient-text z-20">From Idea to Launch</span>
-            </template>
-<template v-slot:description>
-                <p class="z-20">
 
-                    Wondering how your <span class="text-app-primary-glow">vision</span> turns into working
-                    software? <br />We
-                    follow a refined process that starts with
-                    <span class="text-app-secondary-600 ">understanding your goals</span>, <span
-                        class="text-app-primary-glow">followed by clean
-                        design</span>,
-                    <span class="gradient-text2">development, testing, and
-                        final launch</span> — all with
-                    constant collaboration. Discover how we bring ideas to life.
-                </p>
-            </template> -->
-
-            <!-- <template v-slot:title>
-                خطواتنا: <span class="gradient-text z-20">من الفكرة إلى الإطلاق</span>
-            </template>
-            <template v-slot:description>
-                <p class="z-20">
-                    هل تتساءل كيف تتحول <span class="text-app-primary-glow">رؤيتك</span> إلى برنامج يعمل بكفاءة؟<br />
-                    نحن نتبع عملية مدروسة تبدأ بـ
-                    <span class="text-app-secondary-600">فهم أهدافك</span>، تليها
-                    <span class="text-app-primary-glow">تصاميم نظيفة</span>،
-                    ثم <span class="gradient-text2">التطوير، الاختبار، والإطلاق النهائي</span> —
-                    وكل ذلك بالتعاون المستمر.<br />
-                    اكتشف كيف نحول الأفكار إلى واقع.
-                </p>
-            </template> -->
 
             <template v-slot:title>
                 {{ $t('OurProcessTitle') }}
